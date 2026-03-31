750801595737dacc437dcb2c09126b9261896756
Hori <32445087@fukuchiyama.ac.jp>
2026-03-31 14:24:07 +0900

    fix supabase key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://rshalwpuieaivqcibypa.supabase.co";
-const supabaseKey = "あなたのanonキー";
+const supabaseKey = "sb_publishable_95Ck5XegqYHEi6QyzH_JKg_8Riqn8kt";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
