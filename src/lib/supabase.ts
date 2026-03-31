b4289105dbfc7adbadc76a735cad1fd74e5817bc
Hori <32445087@fukuchiyama.ac.jp>
2026-03-31 21:06:58 +0900

    fix supabase env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rshalwpuieaivqcibypa.supabase.co";
-const supabaseKey = "sb_publishable_95Ck5XegqYHEi6QyzH_JKg_8Riqn8kt";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
